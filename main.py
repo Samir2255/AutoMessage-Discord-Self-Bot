6e25a8ff9e616990daeb96e7665901366880dfd5
zaddyinfected <122211933+zaddyinfected@users.noreply.github.com>
2023-05-06 09:19:16 +0000

    Added Max 3 Auto Message and Replaced Minutes with Seconds

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ intents.messages = True
 
 client = discord.Client(intents=intents)
 
-AUTHORIZED_USER_ID = YOUR_USER_ID
+AUTHORIZED_USER_ID = USERID
 
-auto_message_task = None 
+auto_message_tasks = []  # Stores the tasks for sending auto messages
+MAX_AUTO_MESSAGES = 3  # Maximum number of auto messages
 
 @client.event
 async def on_ready():
@@ -16,26 +17,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  
     if message.content.startswith('!set_auto_message'):
         if message.author.id == AUTHORIZED_USER_ID:
             await set_auto_message(message)
         else:
             await message.channel.send("Not authorized to use this command.")
 
-    elif message.content.lower() == 'stop':
+    elif message.content.startswith('stop'):
         if message.author.id == AUTHORIZED_USER_ID:
             await stop_auto_message(message)
 
 async def set_auto_message(message):
-    global auto_message_task
-
-    if auto_message_task is not None and not auto_message_task.done():
-        await message.channel.send("Auto message is already running. Use `stop` to stop it first.")
+    if len(auto_message_tasks) >= MAX_AUTO_MESSAGES:
+        await message.channel.send(f"You have reached the maximum limit of {MAX_AUTO_MESSAGES} auto messages.")
         return
 
-    # Ask the user for the delay in minutes
-    await message.channel.send("Please enter the delay in minutes between each auto message:")
+    # Ask the user for the delay in seconds
+    await message.channel.send("Please enter the delay in seconds between each auto message:")
     
     def check(m):
         return m.author == message.author and m.channel == message.channel
@@ -59,28 +57,45 @@ async def set_auto_message(message):
         return
 
     # Schedule the auto message loop
-    await message.channel.send(f"Auto message set. I will send '{content}' every {delay} minute(s) to channel {target_channel.mention}.")
-    auto_message_task = asyncio.create_task(send_auto_messages(target_channel, content, delay))
+    await message.channel.send(f"Auto message set. I will send '{content}' every {delay} second(s) to channel {target_channel.mention}.")
+    task = asyncio.create_task(send_auto_messages(target_channel, content, delay))
+    auto_message_tasks.append(task)
 
 async def send_auto_messages(channel, content, delay):
     while True:
-        await asyncio.sleep(delay * 60)  # Convert minutes to seconds
+        await asyncio.sleep(delay)
         await channel.send(content)
 
 async def stop_auto_message(message):
-    global auto_message_task
+    global auto_message_tasks
 
-    if auto_message_task is None or auto_message_task.done():
-        await message.channel.send("Auto message is not running.")
+    command_parts = message.content.split()
+    if len(command_parts) == 1:
+        await message.channel.send("Please specify the auto message number to stop (e.g., 'stop 1', 'stop all').")
         return
 
-    auto_message_task.cancel()
-    try:
-        await auto_message_task
-    except asyncio.CancelledError:
-        pass
+    if command_parts[1] == 'all':
+        for task in auto_message_tasks:
+            task.cancel()
+        auto_message_tasks = []
+        await message.channel.send("All auto messages stopped.")
+        return
 
-    await message.channel.send("Auto message stopped.")
+    if len(command_parts) == 2 and command_parts[1].isdigit():
+        index = int(command_parts[1]) - 1
+        if index < 0 or index >= len(auto_message_tasks):
+            await message.channel.send(f"Invalid auto message number. Please specify a valid auto message number (1-{len(auto_message_tasks)}).")
+            return
 
-TOKEN = 'token'
+        task = auto_message_tasks[index]
+        task.cancel()
+        auto_message_tasks.remove(task)
+        task.cancel()
+        auto_message_tasks.remove(task)
+        await message.channel.send(f"Auto message {index + 1} stopped.")
+        return
+
+    await message.channel.send("Invalid command. Please specify the auto message number to stop (e.g., 'stop 1', 'stop all').")
+
+TOKEN = 'your_token_here'
 client.run(TOKEN, bot=False)
